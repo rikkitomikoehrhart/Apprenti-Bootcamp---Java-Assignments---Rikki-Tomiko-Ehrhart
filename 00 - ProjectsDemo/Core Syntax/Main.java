6cc57409ba6707681062601fe945cfed565dd2df
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-05-27 16:22:44 -0500

    Add lecture notes for java variables and operators

---
+++ 00 - ProjectsDemo/Core Syntax/Main.java
@@ -0,0 +1,5 @@
+public class Main { // Starts the Main class
+    public static void main(String[] args) { // The definition for main()
+        System.out.println("Hello World"); // An instruction statement (must end in a ';')
+    }
+}
---
+++ 00 - ProjectsDemo/Variables/src/Main.java
@@ -0,0 +1,56 @@
+public class Main{
+    public static void main(String[] args) {
+//        // We have to declare the data type when declaring variables
+//        int count1, count2, count3;
+//
+//        count1 = 42;
+//        count2 = 13;
+//        count3 = 3;
+//
+//        System.out.println(count1);
+//        System.out.println(count2);
+//        System.out.println(count3);
+//
+//        // Boolean
+//        boolean result = true;
+//
+//        // Single Character
+//        char c = 'c';
+//
+//        // Numerals
+//        // Whole Numbers
+//        byte b;
+//        short s;
+//        int i;
+//        long l;
+//
+//        // Floating Point Numbers
+//        float f;
+//        double d;
+
+        int x = 0;
+
+        x = 1 + 2;
+        x = 4 - 2;
+        x = 4 * 2;
+        x = 4 / 2;
+
+        x += 3;
+        x -= 3;
+        x *= 3;
+        x /= 3;
+
+        x++;
+        x--;
+
+        x = 13;
+
+        System.out.println(x / 2); // returns 6
+
+
+        x = 13;
+        float y = 2.0f;
+
+        System.out.println(x / y); // returns 6.5
+    }
+}
