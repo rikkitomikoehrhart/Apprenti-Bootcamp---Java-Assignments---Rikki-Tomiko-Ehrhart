eb920a3293e949418e3d1581655126929caf7969
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-05-28 12:37:28 -0500

    Update shopping cart exercise to include enum instructions

--- exercise-shopping-cart/src/Main.java
+++ exercise-shopping-cart/src/Main.java
@@ -1,6 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    enum OrderStatus {
+        PENDING,
+        PROCESSING,
+        SHIPPED,
+        DELIVERED
+    }
+
+    enum ShippingStatus {
+        STANDARD,
+        TWO_DAY,
+        OVERNIGHT,
+    }
+
     public static void main(String[] args) {
         // Product Details
         int productID = 1;
@@ -8,6 +21,9 @@ public class Main {
         double productCost = 2.56f;
         double productPrice = 4.99f;
         int productQuantity = 78;
+
+        OrderStatus orderStatus = OrderStatus.PROCESSING;
+        ShippingStatus shipStatus = ShippingStatus.TWO_DAY;
 
         System.out.println("ID: " + productID + "\nCategory: "+ productCategory + "\nCost: $" + productCost + "\nPrice: $" + productPrice + "\nQuantity: " + productQuantity);
 
@@ -23,5 +39,22 @@ public class Main {
         // Potential Profit
         double potentialProfit = profitMargin * productQuantity;
         System.out.println("Potential Profit: $" + potentialProfit);
+
+
+        // Print Statuses
+        System.out.println(ShippingStatus.STANDARD);
+        System.out.println(ShippingStatus.TWO_DAY);
+        System.out.println(ShippingStatus.OVERNIGHT);
+
+        orderStatus = OrderStatus.PROCESSING;
+        System.out.println("Order Status: " + orderStatus);
+
+        System.out.println(OrderStatus.PENDING);
+        System.out.println(OrderStatus.PROCESSING);
+        System.out.println(OrderStatus.SHIPPED);
+        System.out.println(OrderStatus.DELIVERED);
+
+        shipStatus = ShippingStatus.TWO_DAY;
+        System.out.println("Ship Status: " + shipStatus);
     }
 }
