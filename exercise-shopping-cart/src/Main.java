d3612cbb14fb9f26e5bf21bbd54065b18bdce04d
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-05-29 14:27:29 -0500

    Updating Shopping Cart with exercise Shopping Cart IO

--- exercise-shopping-cart/src/Main.java
+++ exercise-shopping-cart/src/Main.java
@@ -1,5 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     enum OrderStatus {
         PENDING,
@@ -15,6 +16,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        Scanner io = new Scanner(System.in);
+
+
         // Product Details
         int productID = 1;
         int productCategory = 2;
@@ -59,5 +63,42 @@ public class Main {
 
         shipStatus = ShippingStatus.TWO_DAY;
         System.out.println("Ship Status: " + shipStatus);
+
+
+        // Exercise Shopping Cart Console IO
+        System.out.print("Are you tax exempt? (Y or y for yes, anything else for no): ");
+        String tax_response = io.nextLine();
+        Boolean taxExempt;
+
+        if (tax_response.equals('y') || tax_response.equals('Y')) {
+            taxExempt = true;
+        } else {
+            taxExempt = false;
+        }
+
+        System.out.println("Which shipping option would you like?");
+        System.out.println("1. Standard (default)");
+        System.out.println("2. Two-Day");
+        System.out.println("3. Overnight");
+        System.out.print("Make a selection : ");
+        int shipSelection = Integer.parseInt(io.nextLine());
+
+        if (shipSelection == 2) {
+            shipStatus = ShippingStatus.TWO_DAY;
+        } else if (shipSelection == 3) {
+            shipStatus = ShippingStatus.OVERNIGHT;
+        } else {
+            shipStatus = ShippingStatus.STANDARD;
+        }
+
+
+        System.out.print("Enter quantity: ");
+        int quantity = Integer.parseInt(io.nextLine());
+
+        System.out.print("Enter Promo Code for free shipping: ");
+        String promoCode = io.nextLine();
+
+
+        System.out.printf("Tax Exempt Status: %b%nShipping Selected: %s%nOrder Quantity: %d%nPromo Code: %s", taxExempt, shipStatus, quantity, promoCode);
     }
 }
