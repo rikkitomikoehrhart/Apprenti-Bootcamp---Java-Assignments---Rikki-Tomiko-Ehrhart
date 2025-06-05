64b5250d4fa9ad6c9c428a2398611ea737ef4909
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-06-05 08:34:48 -0500

    Lab: Text Adventures with the added learned elements

--- lab-text-adventure-through-loops/src/Main.java
+++ lab-text-adventure-through-loops/src/Main.java
@@ -3,53 +3,55 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
+    private static Scanner io = new Scanner(System.in);
     public static void main(String[] args) {
         boolean keyA = false;
         boolean keyB = false;
-        Scanner io = new Scanner(System.in);
         Random random = new Random();
         boolean playing = true;
 
+
+
+        String[] welcomeMessage = {"You are in a grand foyer. In front of you is a Conservatory,", "to the left is a closed door, and to the right is a library,"};
+        String[] wayFinding = {"Where would you like to go?", "1. The Conservatory (forward)", "2. The closed door (left)", "3. The Library (right)"};
+        String[] conservatory = {"\n\nYou entered the Conservatory!", "You are in a sun-drenched room filled with towering tropical", "plants and delicate orchids. The scent of damp earth lingers", "in the air. The glass walls stretch from floor to ceiling, ", "revealing the storm brewing just beyond the garden. \n\n"};
+        String[] lockedStudy = {"\n\nThe door is locked", "Maybe you should explore more...\n\n"};
+        String[] unLockedStudy = {"\n\nYou entered the Study!", "The room stands in still perfection, untouched by time. Rich mahogany", "walls cradle shelves of rare books, which a single beam of moonlight", "falls upon the grand desk -- and the evidence that solves it all. It's", "quiet now... almost too quiet"};
+        String[] wonGameMessage = {"\n\nYou did it! CONGRATULATIONS!"};
+        String[] library = {"\n\nYou entered the Library!", "The room is dim and wood-paneled lined. It has towering shelves", "of dusty, leather-bound books. A fireplace crackles softly beneath", "a stern portrait, casting dancing shadows across the velvet armchairs.", "The air smells of old paper and secrets best left unread.\n\n"};
+
+        String[] explored = {"You've found everything you can here. Going back to the hallway... "};
+        String wouldExplore = "Would you like to explore? (y for yes or anything else for no): ";
+        String keepExploring = "You didn't find anything. Keep exploring? (y for yes or anything else for no): ";
+        String[] yesOrNo = {"y", "Y", "n", "N"};
+
+
+
         while (playing) {
-            System.out.println("You are in a grand foyer. In front of you is a Conservatory,");
-            System.out.println("to the left is a closed door, and to the right is a library.");
+            displayMessage(welcomeMessage);
+            displayMessage(wayFinding);
 
-            System.out.println("\nWhere would you like to go?");
-            System.out.println("1. The Conservatory (forward)");
-            System.out.println("2. The closed door (left)");
-            System.out.println("3. The Library (right)");
-            System.out.print(">>  ");
-            int choice = Integer.parseInt(io.nextLine());
-
+            int choice = promptUserForIntSelection("Enter your selection: ", 3);
 
             switch (choice) {
                 case 1:
                     // Conservatory
-                    System.out.println("\n\nYou entered the Conservatory!");
-                    System.out.println("The room is a sun-drenched room filled with towering tropical");
-                    System.out.println("plants and delicate orchids. The scent of damp earth lingers in");
-                    System.out.println("the air. The glass walls stretch from floor to ceiling, revealing");
-                    System.out.println("the storm brewing just beyond the garden.\n\n");
+                    displayMessage(conservatory);
 
                     if (keyA) {
-                        System.out.print("You've found everything you can here. Going back to the hallway.");
+                        displayMessage(explored);
                         break;
                     } else {
-                        System.out.println("Would you like to explore? (y for yes or anything else for no): ");
-                        String explore;
-                        System.out.print(">>  ");
-                        explore = io.nextLine();
+                        String explore = promptUserForStringSelection(wouldExplore, yesOrNo);
 
                         while (explore.equals("y")) {
                             int randomInt = random.nextInt(10);
                             if (randomInt > 8) {
                                 keyA = true;
-                                System.out.println("You found one half of a key! ");
+                                System.out.println("You found one half of a key! \n\n");
                                 break;
                             } else {
-                                System.out.println("You didn't find anything. Keep exploring? (y for yes or anything else for no): ");
-                                System.out.print(">>  ");
-                                explore = io.nextLine();
+                                explore = promptUserForStringSelection(keepExploring, yesOrNo);
                             }
                         }
                         System.out.println("Going back to the hallway...");
@@ -58,48 +60,34 @@ public class Main {
                 case 2:
                     // The closed door
                     if (!keyA || !keyB) {
-                        System.out.println("\n\nThe door is locked.");
-                        System.out.println("Maybe you should explore more...");
+                        displayMessage(lockedStudy);
                         break;
                     } else if (keyA && keyB) {
-                        System.out.println("\n\nYou entered the Study!");
-                        System.out.println("The room stands in still perfection, untouched by time. Rich mahogany");
-                        System.out.println("walls cradle shelves of rare books, which a single beam of moonlight ");
-                        System.out.println("falls upon the grand desk -- and the evidence that solves it all. It's");
-                        System.out.println("quiet now... almost too quiet.\n\n");
+                        displayMessage(unLockedStudy);
+                        displayMessage(wonGameMessage);
 
-                        System.out.print("You did it! CONGRATULATIONS!");
                         playing = false;
                         break;
                     }
                     break;
                 case 3:
                     // The library
-                    System.out.println("\n\nYou entered the Library!");
-                    System.out.println("The room is dim and wood-paneled lined. It has towering shelves");
-                    System.out.println("of dusty, leather-bound books. A fireplace crackles softly beneath");
-                    System.out.println("a stern portrait, casting dancing shadows across the velvet armchairs.");
-                    System.out.println("The air smells of old paper and secrets best left unread.\n\n");
+                    displayMessage(library);
 
                     if (keyB) {
-                        System.out.print("You've found everything you can here. Going back to the hallway.");
+                        displayMessage(explored);
                         break;
                     } else {
-                        System.out.println("Would you like to explore? (y for yes or anything else for no): ");
-                        String explore;
-                        System.out.print(">>  ");
-                        explore = io.nextLine();
+                        String explore = promptUserForStringSelection(wouldExplore, yesOrNo);
 
                         while (explore.equals("y")) {
                             int randomInt = random.nextInt(10);
                             if (randomInt > 8) {
                                 keyB = true;
-                                System.out.println("You found one half of a key! ");
+                                System.out.println("You found one half of a key! \n\n");
                                 break;
                             } else {
-                                System.out.println("You didn't find anything. Keep exploring? (y for yes or anything else for no): ");
-                                System.out.print(">>  ");
-                                explore = io.nextLine();
+                                explore = promptUserForStringSelection(keepExploring, yesOrNo);
                             }
                         }
                         System.out.println("Going back to the hallway...");
@@ -111,5 +99,66 @@ public class Main {
 
         }
 
+    }
+
+    private static void displayMessage(String[] messages){
+        for (String message : messages) {
+            System.out.println(message);
+        }
+    }
+
+    private static String promptUserForStringSelection(String prompt, String[] validResponses) {
+        String response;
+
+        while (true) {
+            try {
+                System.out.print(prompt);
+                response = io.nextLine();
+                if (response == null || response.trim().isEmpty()) {
+                    throw new IllegalArgumentException("Invalid Response");
+                }
+
+                boolean validResponse = false;
+                for (String valid : validResponses) if (response.equals(valid)) {validResponse = true;}
+
+                if (!validResponse) {
+                    throw new InvalidResponse("Invalid response");
+                }
+
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid response");
+                continue;
+            } catch (InvalidResponse e) {
+                System.out.println("Invalid response.");
+                continue;
+            }
+        }
+
+        return response;
+    }
+
+    private static int promptUserForIntSelection(String prompt, int validResponses) {
+        int response;
+
+        while (true) {
+            try {
+                System.out.print(prompt);
+                response = Integer.parseInt(io.nextLine());
+
+                if (response <= 0 || response > validResponses) {
+                    throw new InvalidResponse("Invalid Response.");
+                }
+                break;
+            } catch (InvalidResponse e) {
+                System.out.println("Invalid choice");
+                continue;
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter the number of your selection.");
+                continue;
+            }
+        }
+
+        return response;
     }
 }
