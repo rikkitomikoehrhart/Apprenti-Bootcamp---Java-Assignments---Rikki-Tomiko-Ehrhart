d99735d8a88912f9fbc16756a3f70b8e2ed80516
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-06-13 12:29:45 -0500

    Update main with imports and needed scanner and random

--- 02 - chatgpt-interface-prompts/magic-girl-battle-cards/src/Main.java
+++ 02 - chatgpt-interface-prompts/magic-girl-battle-cards/src/Main.java
@@ -13,9 +13,15 @@
 //          to call attacks/defense.
 
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
+        
 
 
 
