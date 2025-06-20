747f6306e1816f1608fb843ed045fe796d53ac8c
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-06-20 14:22:23 -0500

    Create MenuOption.java, establish the main menu options, and methods for getting the menu option from the value and getting a list of formatted strings for the menu to display

---
+++ capstone-java-shopping-cart/src/main/java/org/example/model/MenuOption.java
@@ -0,0 +1,41 @@
+package org.example.model;
+
+public enum MenuOption {
+    DISPLAY_CART(1, "Display Cart"),
+    REMOVE_ITEM(2, "Remove an Item"),
+    ADD_ITEM(3, "Add an Item"),
+    CHECKOUT(4, "Checkout"),
+    EXIT(5, "Exit");
+
+    private final int value;
+    private final String description;
+
+    MenuOption(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public static MenuOption getMenuOptionFromValue(int value) {
+        for (MenuOption option : values()) {
+            if (option.value == value) {
+                return option;
+            }
+        }
+
+        return EXIT;
+    }
+
+    public final static String[] getMenuForDisplay() {
+        String[] menu = new String[6];
+
+        menu[0] = "Main Menu";
+
+        MenuOption[] options = values();
+        for (int i = 1; i < menu.length; i++) {
+            MenuOption option = options[i - 1];
+            menu[i] = String.format("%d. %s", option.value, option.description);
+        }
+        return menu;
+    }
+
+}
