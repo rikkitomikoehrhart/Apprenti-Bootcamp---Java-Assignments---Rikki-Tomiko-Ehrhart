85ceb6e846396518f209b1ff15a2fb42c4425d1f
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-07-03 16:12:21 -0500

    implement flight class -- including properties: flightNumber, departureDate, ticketPrice, and aircraft and methods including constructor, getters, setters, and toString()

--- summative-assessment-airport-terminal/src/main/java/org/example/airport/domain/model/Flight.java
+++ summative-assessment-airport-terminal/src/main/java/org/example/airport/domain/model/Flight.java
@@ -1,4 +1,56 @@
 package org.example.airport.domain.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Flight {
+    private String flightNumber;
+    private LocalDate departureDate;
+    private BigDecimal ticketPrice;
+    private Aircraft aircraft;
+
+    public Flight(String flightNumber, LocalDate departureDate, BigDecimal ticketPrice, Aircraft aircraft) {
+        this.flightNumber = flightNumber;
+        this.departureDate = departureDate;
+        this.ticketPrice = ticketPrice;
+        this.aircraft = aircraft;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public BigDecimal getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(BigDecimal ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+    public Aircraft getAircraft() {
+        return aircraft;
+    }
+
+    public void setAircraft(Aircraft aircraft) {
+        this.aircraft = aircraft;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Flight: %s, departing %s.%nAircraft: %s%nTicket Price: $%.2f", this.flightNumber, this.departureDate, this.aircraft, this.ticketPrice);
+    }
 }
+
