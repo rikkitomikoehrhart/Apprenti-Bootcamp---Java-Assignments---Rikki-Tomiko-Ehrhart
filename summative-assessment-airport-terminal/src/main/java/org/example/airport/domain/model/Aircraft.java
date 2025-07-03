3fb237254ffbc6aca6fb825459e2221d1ba49a10
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-07-03 16:28:07 -0500

    Implement base Aircraft class -- model, capacity, and fuelCapacity attributes and methods including constructor, gets, sets, and toString()

--- summative-assessment-airport-terminal/src/main/java/org/example/airport/domain/model/Aircraft.java
+++ summative-assessment-airport-terminal/src/main/java/org/example/airport/domain/model/Aircraft.java
@@ -1,4 +1,42 @@
 package org.example.airport.domain.model;
 
-public interface Aircraft {
+public class Aircraft {
+    private String model;
+    private int capacity;           // of passengers
+    private double fuelCapacity;    // in liters
+
+    public Aircraft(String model, int capacity, double fuelCapacity) {
+        this.model = model;
+        this.capacity = capacity;
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(double fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Model: %s%nPassenger Capacity: %f%nFuel Capacity (in liters): %f%n", this.model, this.capacity, this.fuelCapacity);
+    }
 }
