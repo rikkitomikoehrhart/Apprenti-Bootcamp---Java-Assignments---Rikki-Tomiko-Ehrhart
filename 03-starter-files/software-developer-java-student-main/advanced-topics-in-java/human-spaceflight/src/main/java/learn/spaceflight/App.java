adf9ba90bd70ab26e3089e7537d76dcbe1770221
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-07-03 09:12:00 -0500

    Spring lessons and code along

--- 03-starter-files/software-developer-java-student-main/advanced-topics-in-java/human-spaceflight/src/main/java/learn/spaceflight/App.java
+++ 03-starter-files/software-developer-java-student-main/advanced-topics-in-java/human-spaceflight/src/main/java/learn/spaceflight/App.java
@@ -1,8 +1,47 @@
 package learn.spaceflight;
 
+import learn.spaceflight.personnel.Astronaut;
+import learn.spaceflight.spacecraft.InterstellarTransport;
+import learn.spaceflight.spacecraft.MoonHopper;
+import learn.spaceflight.spacecraft.Probe;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 
     public static void main(String[] args) {
-        // Spring code here...
+
+        // 1. An ApplicationContext is the DI container         // [DI = Dependency Injection]
+        // 2. The ClassPathXmlApplicationContext is the specific type of DI container
+        //      It requires an XML configuration file to register beans or object instances.
+        //      Here, we use "dependency-configuration.xml" but the file can be called whatever
+        //      we like as long as it uses the correct XML schema.
+        ApplicationContext container = new ClassPathXmlApplicationContext("dependency-configuration.xml");
+
+        // 3. The container knows about an Astronaut, so we ask it for one.
+//        Astronaut captain = container.getBean("captain", Astronaut.class);
+//        System.out.println("Captain: " + captain);
+//
+//        Astronaut notTheCaptain = container.getBean("crew", Astronaut.class);
+//        System.out.println("Not the Captain: " + notTheCaptain);
+
+//        MoonHopper hopper = container.getBean(MoonHopper.class);
+//        System.out.println(hopper);
+
+//        InterstellarTransport transport = container.getBean(InterstellarTransport.class);
+//        System.out.println(transport);
+
+        Probe one = container.getBean(Probe.class);
+        Probe two = container.getBean(Probe.class);
+
+        // Add distance to one
+        one.addDistance(1000);
+        System.out.println("One: " + one);
+        System.out.println("Two: " + two);
+
+        // Add distance to two
+        two.addDistance(2589);
+        System.out.println("One: " + one);
+        System.out.println("Two: " + two);
     }
 }
--- 03-starter-files/software-developer-java-student-main/advanced-topics-in-java/memories/src/main/java/learn/memories/App.java
+++ 03-starter-files/software-developer-java-student-main/advanced-topics-in-java/memories/src/main/java/learn/memories/App.java
@@ -5,25 +5,38 @@ import learn.memories.domain.MemoryService;
 import learn.memories.ui.ConsoleIO;
 import learn.memories.ui.Controller;
 import learn.memories.ui.View;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
-        // "./data/memories.txt" is the path to our "production" data file.
-        MemoryFileRepository repository = new MemoryFileRepository("./data/memories.txt");
-        // repository satisfies the MemoryRepository dependency.
-        MemoryService service = new MemoryService(repository);
+        ApplicationContext context = new ClassPathXmlApplicationContext("dependency-configuration.xml");
 
-        ConsoleIO io = new ConsoleIO();
-        // io satisfies the TextIO dependency.
-        View view = new View(io);
-
-        // view satisfies the View dependency
-        // service satisfies the MemoryService dependency
-        Controller controller = new Controller(view, service);
+        Controller controller = context.getBean(Controller.class);
 
         // Run the app!
         controller.run();
+
+
+
+
+
+        //        // "./data/memories.txt" is the path to our "production" data file.
+//        MemoryFileRepository repository = new MemoryFileRepository("./data/memories.txt");
+//        // repository satisfies the MemoryRepository dependency.
+//        MemoryService service = new MemoryService(repository);
+//
+//        ConsoleIO io = new ConsoleIO();
+//        // io satisfies the TextIO dependency.
+//        View view = new View(io);
+//
+//        // view satisfies the View dependency
+//        // service satisfies the MemoryService dependency
+//        Controller controller = new Controller(view, service);
+//
+//        // Run the app!
+//        controller.run();
     }
 
 }
