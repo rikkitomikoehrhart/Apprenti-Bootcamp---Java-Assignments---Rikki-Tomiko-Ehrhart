3177ec6b11da1f33701f18ddfc3e6f920a620661
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-07-03 16:34:47 -0500

    implement CommericalAircraft class extending Aircraft and adding a airlineName property and methods of constructor, gets, sets, and toString()

--- summative-assessment-airport-terminal/src/main/java/org/example/airport/domain/model/CommericalAircraft.java
+++ summative-assessment-airport-terminal/src/main/java/org/example/airport/domain/model/CommericalAircraft.java
@@ -1,4 +1,24 @@
 package org.example.airport.domain.model;
 
-public class CommericalAircraft {
+public class CommericalAircraft extends Aircraft {
+    private String airlineName;
+
+    public CommericalAircraft(String model, int capacity, double fuelCapacity, String airlineName) {
+        super(model, capacity, fuelCapacity);
+
+        this.airlineName = airlineName;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Airline: %s%n", this.airlineName) + super.toString();
+    }
 }
