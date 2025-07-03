ffc0ab44cc2a9c257eb6b20c88df61802a005853
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-07-03 16:40:35 -0500

    implement PrivateJet class extending Aircraft -- additional attributes hasLuxuryService and maxSpeed an methods constructor, gets, sets, and toString()

--- summative-assessment-airport-terminal/src/main/java/org/example/airport/domain/model/PrivateJet.java
+++ summative-assessment-airport-terminal/src/main/java/org/example/airport/domain/model/PrivateJet.java
@@ -1,4 +1,34 @@
 package org.example.airport.domain.model;
 
-public class PrivateJet {
+public class PrivateJet extends Aircraft {
+    private boolean hasLuxuryService;
+    private int maxSpeed;
+
+    public PrivateJet(String model, int capacity, double fuelCapacity, boolean hasLuxuryService, int maxSpeed) {
+        super(model, capacity, fuelCapacity);
+
+        this.hasLuxuryService = hasLuxuryService;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public boolean isHasLuxuryService() {
+        return hasLuxuryService;
+    }
+
+    public void setHasLuxuryService(boolean hasLuxuryService) {
+        this.hasLuxuryService = hasLuxuryService;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format("Luxury Service: %b%nMax Speed: %f%n", this.hasLuxuryService, this.maxSpeed);
+    }
 }
