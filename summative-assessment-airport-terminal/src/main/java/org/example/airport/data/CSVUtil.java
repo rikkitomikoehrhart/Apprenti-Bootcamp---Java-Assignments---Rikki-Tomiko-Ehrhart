1df16e916f3e2dab562c77f933703078bb394bbe
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-07-07 11:10:47 -0500

    Implement CSV Reading from reservations.csv and populating the Reservation System

--- summative-assessment-airport-terminal/src/main/java/org/example/airport/data/CSVUtil.java
+++ summative-assessment-airport-terminal/src/main/java/org/example/airport/data/CSVUtil.java
@@ -1,11 +1,14 @@
 package org.example.airport.data;
 
 import com.opencsv.CSVWriter;
-import org.example.airport.domain.model.Flight;
-import org.example.airport.domain.model.Passenger;
+import com.opencsv.CSVReader;
+import org.example.airport.domain.model.*;
 import org.example.airport.domain.reservation.ReservationSystem;
 
 import java.io.FileWriter;
+import java.io.FileReader;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public class CSVUtil {
@@ -18,11 +21,7 @@ public class CSVUtil {
     }
 
     public void saveReservationsToCSV() {
-        String[] header = {"flightNumber", "departureDate", "ticketPrice", "passengerName", "passportNumber", "aircraftModel", "aircraftType"};
-
         try (CSVWriter writer = new CSVWriter(new FileWriter(PATH))) {
-            writer.writeNext(header);
-
             List<String> flightNumbers = reservationSystem.getFlightNumbers();
 
             for (String flightNumber : flightNumbers) {
@@ -40,10 +39,36 @@ public class CSVUtil {
             }
 
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("ERROR: " + e);
         }
 
+    }
 
+
+    public void loadReservationsFromCSV() {
+        try (CSVReader reader = new CSVReader(new FileReader(PATH))) {
+            List<String[]> records = reader.readAll();
+
+            for (String[] record : records) {
+                Aircraft aircraft;
+
+                if (record[6].equals("CommercialAircraft")) {
+                    aircraft = new CommercialAircraft(record[5], 0, 0 , "");
+                } else {
+                    aircraft = new PrivateJet(record[5], 0, 0, false, 0);
+                }
+                
+                Flight flight = new Flight(record[0], LocalDate.parse(record[1]), BigDecimal.valueOf(Long.parseLong(record[2])), aircraft);
+                Passenger passenger = new Passenger(record[3], record[4]);
+
+
+                reservationSystem.addFlight(flight);
+                reservationSystem.addReservation(flight.getFlightNumber(), passenger);
+
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e);
+        }
     }
 
 }
