92242b591ede569e4432bcc9d226a8ff128c557c
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-07-08 11:18:18 -0500

    Init Commit - Assessment Java Console App Inventory Manager - Maven and Spring Boot

---
+++ Inteventory-Manager/src/main/java/com/example/Inteventory/Manager/InteventoryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Inteventory.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteventoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteventoryManagerApplication.class, args);
+	}
+
+}
---
+++ Inteventory-Manager/src/test/java/com/example/Inteventory/Manager/InteventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Inteventory.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
