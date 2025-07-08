0f4751ae5b4a5833e9cc8b56213e98119c360ed8
rikkitomikoehrhart <rikki.ehrhart@g.austincc.edu>
2025-07-08 15:12:35 -0500

    Add ui utils and view functions to display perishable product information

--- Inventory-Manager/src/main/java/com/example/Inventory/Manager/ui/UIUtils.java
+++ Inventory-Manager/src/main/java/com/example/Inventory/Manager/ui/UIUtils.java
@@ -1,4 +1,37 @@
 package com.example.Inventory.Manager.ui;
 
+import java.util.Scanner;
+
 public class UIUtils {
+    Scanner scanner = new Scanner(System.in);
+
+    public void println(Object value) {
+        System.out.println(value);
+    }
+
+    public void print(Object value) {
+        System.out.print(value);
+    }
+
+    public void printf(String format, Object... values) {
+        System.out.printf(format, values);
+    }
+
+    public String readString(String prompt) {
+        print(prompt);
+        return scanner.nextLine();
+    }
+
+    public int readInt(String prompt) {
+        while (true) {
+            String value = readString(prompt);
+            try {
+                int result = Integer.parseInt(value);
+                return result;
+            } catch (NumberFormatException e) {
+                printf("`%s` is not a valid number", value);
+            }
+        }
+    }
+
 }
--- Inventory-Manager/src/main/java/com/example/Inventory/Manager/ui/View.java
+++ Inventory-Manager/src/main/java/com/example/Inventory/Manager/ui/View.java
@@ -1,4 +1,14 @@
 package com.example.Inventory.Manager.ui;
 
+import com.example.Inventory.Manager.model.PerishableProduct;
+
 public class View {
+    UIUtils uiUtils = new UIUtils();
+    private final String DISPLAY_PERISHABLE_PRODUCT = "%10-s | %-20s | %-10s | $%10.2f | %s";
+
+
+
+    public void displayPerishableProduct(PerishableProduct product) {
+        uiUtils.printf(DISPLAY_PERISHABLE_PRODUCT, product.getProductID(), product.getProductName(), product.getQuantity(), product.getPrice(), product.getExpirationDate());
+    }
 }
